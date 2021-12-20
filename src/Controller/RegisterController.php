643c9b6bb6fb072e3daeafbc5feffc473f6892ad
jonathan <jonathan.dupau24@gmail.com>
2021-12-20 17:36:06 +0100

    ADD login form + hached password + contraints on forms

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -4,24 +4,55 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegisterType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RegisterController extends AbstractController
 {
+    // integre doctrine sous la variable $entityManager
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager) {
+        $this->entityManager = $entityManager;
+    }
+
+
     /**
      * @Route("/inscription", name="register")
      */
-    public function index(): Response
+    public function index(Request $request, UserPasswordEncoderInterface $encoder): Response
     {
-
+        // on utilise le parametre UserPasswordEncoderInterface pour utiliser notre methode d'encryptage de mot de passe 
         $user = new User();
+        // création du formulaire
         $form = $this->createForm(RegisterType::class, $user);
 
+        // récupération du l'objet request au formulaire
+        $form->handleRequest($request);
+
+        //  si le formulaire es soumis et validé 
+        if ($form->isSubmitted() && $form->isValid()) {
+            // on récupére la data du form
+            $user = $form->getData();
+
+            // encodage du mot de passe que l'on recupere grace au geter
+            $password = $encoder->encodePassword($user, $user->getPassword());
+            
+            // envoi du mot de passe crypter grace au seter
+            $user->setPassword($password);
+
+            // ON ENVOIE la data en bdd
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+        }
     
         return $this->render('register/index.html.twig', [
             'controller_name' => 'RegisterController',
+            // on créer la vue du formulaire
             'form' => $form->createView()
         ]);
     }
--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -6,10 +6,12 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class RegisterType extends AbstractType
 {
@@ -17,37 +19,64 @@ class RegisterType extends AbstractType
     {
         $builder // ce sont des inputs ci dessous
         ->add('firstname', TextType::class, [
-            'label' => 'Votre prénom',  // l'import de TextType::class + le tableau juste aprés sert à rajouter différent parametre à notre form , ici pour changer le nom de notre label
+            // l'import de TextType::class + le tableau juste aprés sert à rajouter différent parametre à notre form , ici pour changer le nom de notre label
+            'label' => 'Votre prénom',
+            // ajout de contraintes, ici le nombre minimum et maximum de caractere
+            'constraints
+            ' =>  new Length([
+                'min' => 2,
+                'max' => 50
+                ]
+            ),
             'attr' => [
                 'placeholder' => 'Merci de saisir votre prénom'
             ]
         ])
         ->add('lastname', TextType::class, [
             'label' => 'Votre nom',
+            'constraints' =>  new
+            Length(
+                [
+                    'min' => 2,
+                    'max' => 50
+                ]
+            ),
             'attr' => [
                 'placeholder' => 'Merci de saisir votre nom'
             ]
         ])
         ->add('email', EmailType::class, [
             'label' => 'Votre Email',
+            'constraints' =>  new
+            Length(
+                [
+                    'min' => 2,
+                    'max' => 50
+                ]
+            ),
+
             'attr' => [
                 'placeholder' => 'Merci de saisir votre Email'
             ]
         ]) 
-        // ->add('roles') en comm car on ne souhaite pas que l'utilisateur choissise son role
-        ->add('password', PasswordType::class, [
-            'label' => 'Votre Mot de passe',
-            'attr' => [
-                'placeholder' => 'Merci de saisir votre Mot de passe'
-            ]
-        ])
-            ->add('password_confirm', PasswordType::class, [
-                'label' => 'Confirmez Votre Mot de passe',
-                'mapped' => false,
+        ->add('password', RepeatedType::class, [
+            'type' => PasswordType::class,
+            // message d'erreur lorsque les champs ne sont pas identique
+            'invalid_message' => 'le mot de passe ne correspond pas',
+            'required' => true,
+            'first_options' => [
+                'label' => 'Mot de Passe',
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre Mot de passe'
+                ]
+            ],
+            'second_options' => [ 
+                'label' => 'Confirmez votre mot de passe',
                 'attr' => [
                     'placeholder' => 'Merci de confirmer votre Mot de passe'
                 ]
-            ])
+            ]
+        ])
         ->add('submit', SubmitType::class, [
             'label' => "S'inscire",
         ])
